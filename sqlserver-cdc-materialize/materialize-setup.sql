252f283fbc617825db570da2d4f825608f2577f1
Dani Palma <dani@estuary.dev>
2024-05-29 12:19:03 +0100

    update materialize example

--- sqlserver-cdc-materialize/materialize-setup.sql
+++ sqlserver-cdc-materialize/materialize-setup.sql
@@ -23,41 +23,21 @@ CREATE SOURCE sqlserver_sales
   FORMAT AVRO USING CONFLUENT SCHEMA REGISTRY CONNECTION csr_estuary_connection
     ENVELOPE UPSERT;
 
-CREATE INDEX sqlserver_sales_idx on sqlserver_sales (sale_id);
+create or replace view sales_anomalies as
+  with recent_sales as (
+    select * from sqlserver_sales
+    where mz_now() <= sale_date + interval '7 days'
+  ),
+  rolling_avg as (
+    select
+    customer_id,
+    avg(total_price) as customer_spend_avg
+    from recent_sales
+    group by customer_id
+  )
+  select
+    a.customer_id, a.customer_spend_avg, s.product_id, s.quantity, s.sale_date, s.sale_id, s.total_price, s.unit_price
+  from recent_sales s join rolling_avg a using(customer_id)
+  where s.total_price > 1.5 * a.customer_spend_avg;
 
-CREATE OR REPLACE MATERIALIZED VIEW estuary_cnt_sales AS
-SELECT customer_id, count(sale_id)
-FROM sqlserver_sales
-GROUP BY customer_id;
-
-CREATE MATERIALIZED VIEW estuary_mv_total_sales_per_customer AS
-SELECT customer_id, SUM(total_price) AS total_sales_amount
-FROM sqlserver_sales
-GROUP BY customer_id;
-
-CREATE MATERIALIZED VIEW estuary_mv_average_unit_price_per_product AS
-SELECT product_id, AVG(unit_price) AS average_unit_price
-FROM sqlserver_sales
-GROUP BY product_id;
-
-CREATE MATERIALIZED VIEW estuary_mv_total_sales_quantity_per_product AS
-SELECT product_id, SUM(quantity) AS total_sales_quantity
-FROM sqlserver_sales
-GROUP BY product_id;
-
-CREATE MATERIALIZED VIEW estuary_mv_total_sales_per_day AS
-SELECT DATE(sale_date) AS sale_day, SUM(total_price) AS total_sales_amount
-FROM sqlserver_sales
-GROUP BY DATE(sale_date);
-
-CREATE MATERIALIZED VIEW estuary_mv_top_selling_products_by_quantity AS
-SELECT product_id, SUM(quantity) AS total_quantity_sold
-FROM sqlserver_sales
-GROUP BY product_id
-ORDER BY total_quantity_sold DESC
-LIMIT 5;
-
-CREATE MATERIALIZED VIEW estuary_mv_total_sales_per_month AS
-SELECT date_part('year', sale_date) AS sale_year, date_part('month', sale_date) AS sale_month, SUM(total_price::float) AS total_sales_amount
-FROM sqlserver_sales
-GROUP BY date_part('year', sale_date), date_part('month', sale_date);
+create index on sales_anomalies (customer_id);
