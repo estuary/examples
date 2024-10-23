a9b2e881cfede7f1223babf50497cb11a8970979
Shruti Mantri <smantri@moveworks.ai>
2024-10-23 17:54:03 +0530

    Add datagen and streamlit application for MongoDB Pinecone RAG

---
+++ mongodb-pinecone-rag/rag.py
@@ -0,0 +1,34 @@
+import os
+import pinecone
+from dotenv import load_dotenv
+from llama_index.core.chat_engine.condense_plus_context import (
+    CondensePlusContextChatEngine,
+)
+from llama_index.llms.openai import OpenAI
+from llama_index.core.llms import ChatMessage, MessageRole
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from llama_index.core import VectorStoreIndex
+
+load_dotenv()
+
+
+pinecone_index = pinecone.Index(
+    api_key=os.environ.get("PINECONE_API_KEY"),
+    host=os.environ.get("PINECONE_HOST"),
+)
+
+vector_store = PineconeVectorStore(pinecone_index=pinecone_index, namespace="reviews", text_key="flow_document")
+
+retriever = VectorStoreIndex.from_vector_store(vector_store).as_retriever(
+    similarity_top_k=5
+)
+
+llm = OpenAI(model="gpt-3.5-turbo")
+
+chat_engine = CondensePlusContextChatEngine.from_defaults(
+    retriever=retriever,
+    system_prompt="""You are a helpful chat bot that answers users questions releated to reviews on ecommerce products. If you receive an unrelated question; just say "I don't know".
+    """,
+    verbose=True,
+    llm=llm,
+)
